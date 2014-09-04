9e0e4ca31fe8024d7ec5e94d8b47894b032864fe
Tuomo Mäkelä <tuomo.makela@gmail.com>
2014-09-04 19:51:43 +0300

    merging changes back to MapController

--- game/mapcontroller.js
+++ game/mapcontroller.js
@@ -9,29 +9,49 @@ function MapController( boardModel ) {
 		this.board.activeBoard[x][y] = element;
 	}
 
-	this.checkValid = function ( x, y ) {
-		if( square_type === undefined || square_type == "optional" || square_type == element.substr(2)){
-			return true;
-		} else {
-			if(square_type.substr(0,2) == "b_"){
-				graph.printFeedBack("Can't put a block on top of another block!");
+	this.checkValid = function ( x, y, element ) {
+		this.checkValid = function ( x, y, element ) {
+			square_type = this.board.activeBoard[x][y];
+			if( square_type === undefined || square_type == "optional" || square_type == element.substr(2)){
+				return true;
+			} else {
+				if(square_type.substr(0,2) == "b_"){
+					graph.printFeedBack("Can't put a block on top of another block!");
+				}
+				else if(square_type == "blank"){
+					graph.printFeedBack("Blank squares must be left empty!");
+				}
+				else{
+					graph.printFeedBack("Block must be completely inside game area!");
+				}
+				return false;
 			}
-			else if(square_type == "blank"){
-				graph.printFeedBack("Blank squares must be left empty!");
-			}
-			else{
-				graph.printFeedBack("Block must be completely inside game area!");
-			}
-			return false;
 		}
 	}
 	
+	this.checkSuccess = function ( ){
+		var x_max = this.board.size.x;
+		var y_max = this.board.size.y;
+		var elements = ["fire","water","air","earth"];
+		var success = true;
+
+		for ( y = 0; y < y_max; y++ ){
+			for ( x = 0; x < x_max; x++ ){
+				var square_type = this.board.activeBoard[x][y];
+				if (square_type === undefined || elements.indexOf(square_type) > -1 ){
+					success = false;
+				}
+			}
+		}
+		return success;
+ 	}
+	
 	this.addBlockToModel = function( element, x1, y1, x2, y2, x3, y3, x4, y4 )  {
 		// Check that block can be placed 
-		if (  this.checkValid(x1, y1)  &&
-			this.checkValid(x2, y2)  &&
-			this.checkValid(x3, y3) &&
-			this.checkValid(x4, y4) ) {
+		if (  this.checkValid(x1, y1, element)  &&
+			this.checkValid(x2, y2, element)  &&
+			this.checkValid(x3, y3, element) &&
+			this.checkValid(x4, y4, element) ) {
 			this.board.activeBoard[x1][y1] = element;
 			this.board.activeBoard[x2][y2] = element;
 			this.board.activeBoard[x3][y3] = element;
